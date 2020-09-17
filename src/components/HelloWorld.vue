9375c942013ff1939253ff4cd6fe0388529d4fe7
kennylam91 <phamvanlam1991@gmail.com>
2020-09-17 11:58:38 +0700

    done demo for vuelidate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,278 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div style="margin: 2rem auto; width: 400px;">
+    <div style="display: flex; margin-bottom: 2rem;">
+      <div style="margin-right: 2rem;">
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Name of practice or clinic"
+          v-model="$v.clinic.name.$model"
+        />
+        <div
+          class="error"
+          v-if="!$v.clinic.name.required && $v.clinic.name.$anyDirty"
+        >
+          Name is required.
+        </div>
+      </div>
+      <div >
+        <input
+          class="form-control"
+          placeholder="Type of practice or clinic"
+          v-model="$v.clinic.type_clinic.$model"
+        />
+        <div
+          class="error"
+          v-if="
+            !$v.clinic.type_clinic.required && $v.clinic.type_clinic.$anyDirty
+          "
+        >
+          Type of practice or clinic is required.
+        </div>
+      </div>
+    </div>
+    <div style="display: flex; margin-bottom: 2rem;">
+      <div style="margin-right: 2rem;">
+        <input
+          class="form-control"
+          type="email"
+          placeholder="Email address"
+          v-model="$v.clinic.email.$model"
+        />
+        <div
+          class="error"
+          v-if="!$v.clinic.email.required && $v.clinic.email.$anyDirty"
+        >
+          Email address is required.
+        </div>
+        <div
+          class="error"
+          v-if="!$v.clinic.email.email && $v.clinic.email.$anyDirty"
+        >
+          Email is invalid
+        </div> 
+      </div>
+      <div >
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Phone Number"
+          v-model="$v.clinic.phone.$model"
+        />
+        <div
+          class="error"
+          v-if="!$v.clinic.phone.required && $v.clinic.phone.$anyDirty"
+        >
+          Phone number is required.
+        </div>
+      </div>
+    </div>
+
+    <div style="display: flex; margin-bottom: 2rem;">
+      <div style="margin-right: 2rem;">
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Website"
+          v-model="$v.clinic.website.$model"
+        />
+        <div
+          class="error"
+          v-if="!$v.clinic.website.required && $v.clinic.website.$anyDirty"
+        >
+          Website is required.
+        </div>
+      </div>
+      <div >
+        <input
+          class="form-control"
+          type="number"
+          placeholder="Years in Business"
+          v-model="$v.clinic.year_in_business.$model"
+        />
+        <div
+          class="error"
+          v-if="
+            !$v.clinic.year_in_business.required &&
+              $v.clinic.year_in_business.$anyDirty
+          "
+        >
+          Years in business is required.
+        </div>
+      </div>
+    </div>
+
+    <div style="display: flex; margin-bottom: 2rem;">
+      <div style="margin-right: 2rem;">
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Contact Person"
+          v-model="$v.clinic.contact_person.$model"
+        />
+        <div
+          class="error"
+          v-if="
+            !$v.clinic.contact_person.required &&
+              $v.clinic.contact_person.$anyDirty
+          "
+        >
+          Contact person is required.
+        </div>
+      </div>
+      <div >
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Title"
+          v-model="$v.clinic.title.$model"
+        />
+        <div
+          class="error"
+          v-if="!$v.clinic.title.required && $v.clinic.title.$anyDirty"
+        >
+          Title is required.
+        </div>
+      </div>
+    </div>
+
+    <div class="row mb-40">
+      <!-- <div class="col-sm-6">
+        <multiselect
+          v-model="selectedCountry"
+          :options="countries"
+          :searchable="true"
+          :close-on-select="true"
+          :show-labels="false"
+          track-by="id"
+          label="name"
+          placeholder="Country"
+        ></multiselect>
+      </div> -->
+      <!-- <div class="col-sm-6">
+        <multiselect
+          v-model="selectedCity"
+          :searchable="true"
+          :close-on-select="true"
+          :show-labels="false"
+          :loading="isLoading"
+          track-by="short_name"
+          label="name"
+          @search-change="searchCity"
+          :show-no-results="false"
+          :clear-on-select="false"
+          :internal-search="false"
+          open-direction="bottom"
+          :options="cities"
+          placeholder="City"
+        ></multiselect>
+      </div> -->
+    </div>
+
+    <div class="row mb-40">
+      <!-- <div class="col-sm-12">
+        <div class="title-22">Languages Spoken</div>
+        <div
+          v-for="(language, idx) in languages"
+          :key="idx"
+          class="form-check form-check-inline"
+        >
+          <input
+            v-model="clinic.language_spokens"
+            class="form-check-input"
+            type="checkbox"
+            :id="'chk__' + idx"
+            :value="language"
+          />
+          <label class="form-check-label" :for="'chk__' + idx">{{
+            language
+          }}</label>
+        </div>
+      </div> -->
+    </div>
+
+    <div class="row mt-20">
+      <div class="col-sm-12">
+        <button
+          class="btn btn-danger btn-round pl-50 pr-50"
+          :disabled="$v.$invalid"
+          @click.prevent="onSendFeedback"
+        >
+          Submit
+        </button>
+        <span class="ml-3 text-primary title-16" v-if="isSubmited"
+          >Message Sent</span
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, email } from "vuelidate/lib/validators";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  mixins: [validationMixin],
+  data() {
+    return {
+      clinic: {
+        name: "",
+        type_clinic: "",
+        email: "",
+        website: "",
+        year_in_business: "",
+        contact_person: "",
+        phone: "",
+        // country: "",
+        title: "",
+        // city: "",
+        // language_spokens: [],
+      },
+      isSubmited: false
+    }
+  },
+  validations: {
+    clinic: {
+      name: { required },
+      type_clinic: { required },
+      email: { required, email },
+      website: { required },
+      year_in_business: { required },
+      contact_person: { required },
+      phone: { required },
+      // country: { required },
+      title: { required },
+      // city: { required },
+      // language_spokens: { required },
+    },
+  },
+  methods: {
+    onSendFeedback(){
+      console.log('onSendFeedback');
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+input{
+  height: 36px;
+  width: 300px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+button{
+  height: 36px;
+  width: 70px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.error {
+  color: #ff00008c;
 }
-a {
-  color: #42b983;
+
+.error:focus {
+  outline-color: #f99;
 }
 </style>
